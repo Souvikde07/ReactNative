592abdc559b7c9bf1ce4f98e8c2823a0a4f0d246
Souvik <souvik.de@sato-global.com>
2025-02-12 10:50:37 +0900

    Push all of lesson01 and lesson02

---
+++ lesson02/MyApp/app/(tabs)/index.tsx
@@ -0,0 +1,31 @@
+import { View, Text, StyleSheet,ImageBackground } from 'react-native'
+import React from 'react'
+import icedCoffee from './assets/splash-icon.png'
+
+const app = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+      source={}
+      >
+      <Text style={styles.text}>Coffee Shop</Text>
+      </ImageBackground>
+    </View>
+  )
+}
+
+export default app
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+  text: {
+    color: 'black',
+    fontSize: 42,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  }
+})
